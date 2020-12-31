b60118263e47cad8ddbf0cb8c20c43395b5df960
areejobaid94 <areej.obaid@yahoo.com>
2020-12-31 15:12:49 +0200

    done

--- js/app.js
+++ js/app.js
@@ -15,7 +15,6 @@ function welcomeMess() {
 
 //// about me que
 function startGame() {
-    console.log("start")
     alert('answer yes or no');
     point = 0;
     let queArr = ["q1. Areej's family name is Abbad, answer yes or no", "q2. Areej is a Civil engineer, answer yes or no", "q3. Areej worked as a windows developer, answer yes or no", "q4. Areej worked for 2 years as a developer, answer yes or no", "q5. Areej Joined the caurse because she want to learn more about web development, answer yes or no"]
@@ -23,7 +22,6 @@ function startGame() {
     for (var i = 0; i < queArr.length; i++) {
         que(queArr[i], ansArr[i]);
     }
-    console.log(point);
     alert(`${person} You have got ${point}/5`)
     document.getElementById('points').textContent = `${person} You have got ${point}/5`;
 }
@@ -59,13 +57,12 @@ function answerIsNo(qName, answer) {
 function startIQTest() {
     point = 0;
     let queArr = ["complete the series => 4 - 7 - 12 - 15 - 20  - ...", "complete the series => 2 - 0 - 4 - 0 - 8 - 0 - ...", "complete the series => 18 - 10 - 6 - 4 - ...", "complete the series => 1024 - 512 - 256 - 128 - ...", "complete the series => 2 - 4 - 8 - 16 - ...", "complete the series => 2 - 5 - 11 - ..."]
-    let ansArr = [23, 16, 3, 64, 32, 29]
+    let ansArr = [23, 16, 3, 64, 32, 20];
     for (var i = 0; i < queArr.length; i++) {
         testQue(queArr[i], ansArr[i]);
     }
     alert(`${person} You have got ${point}/6`)
     document.getElementById('points-iq').textContent = `${person} You have got ${point}/6`;
-    console.log(point);
 }
 
 function testQue(str, correctAnswer) {
@@ -86,7 +83,6 @@ function testQue(str, correctAnswer) {
 
 function MultCorrectAnsQue(str, correctAnswer) {
     let q = prompt(str);
-    console.log(correctAnswer);
     let i = 0;
     while (correctAnswer.includes(q.toLocaleLowerCase()) == false && i < 5) {
         alert(`wrong answer, you have ${5 - i} attempts`)
@@ -103,7 +99,6 @@ function MultCorrectAnsQue(str, correctAnswer) {
 
 /// Level2
 function startGameLevel2() {
-    console.log('hi')
     point = 0;
     let queArr = ["How many cats do I have", "How many brothers do I have", "How many sisters do I have", "How many years of experience in software engineering do I have", "How old am I", "How many languages I can speech"]
     let ansArr = [1, 1, 3, 1, 26, 2]
@@ -112,7 +107,6 @@ function startGameLevel2() {
     }
     alert('The final question!');
     MultCorrectAnsQue("What are my favorite colors, guess one", ['black', 'blue', 'white', 'green'])
-    console.log(point);
     if (point > 3) {
         alert(`${person}, Done you have got ${point}/7, too high`)
     } else {
